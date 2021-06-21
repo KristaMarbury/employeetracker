d1fa924e7c5486356feb149a686b4ef2dbea229d
Krista Marbury <laetaiustus@gmail.com>
2021-06-21 13:25:53 -0500

    updated the seeds file wont download on doplhins for some reason

--- sql_folder/seeds.sql
+++ sql_folder/seeds.sql
@@ -1,9 +1,29 @@
 use employees;
-
+-- new problem... this won't work on dolphins for some reason 
 insert into employee (first_name, last_name, role_id, manager_id)
 values
-('michael', 'scott', 1, null),
-('dwight', 'shrute', 2, 1),
-('jim', 'halpert', 2, 1),
-('creed', 'smith', 2, 1),
-('robert', 'california', 2, 1);
+('Tim', 'Doe', 'Sales Lead', 'Asheley Rodriguez'),
+('Mike', 'Chan', 'Salesperson', 'Time Doe'),
+('Ashley', 'Rodriguez', 'Lead Engineer', 'nobody'),
+('Malia', 'Brown', 'Accountant', 'nobody'),
+('Sarah', 'Lourd', 'Legal Team Lead', 'nobody'),
+('Tom', 'Allen', 'Layer', 'Sarah Lourd'),
+('Christian', 'Eckenrode', 'Lead Engineer', 'Mike Chan');
+
+insert into roles (title, salary, department_id)
+values
+('Sales Lead', 100000, 'Sales'),
+('Salesperson', 80000, 'Sales'),
+('Lead Engineer', 150000, 'Engineering'),
+('Software Engineer', 120000, 'Engineering'),
+('Accountant', 125000, 'Finance'),
+('Legal Tam Lead', 250000, 'Legal'),
+('Layer', 190000, 'Legal'),
+('Lead Engineer', 150000, 'Engineering');
+
+insert into department (name)
+values
+('Sales'),
+('Engineering'),
+('Finance'),
+('Legal');
