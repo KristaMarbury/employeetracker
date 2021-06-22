e1358ae28b69b7f7cff56c7cdf1e6cfcfe242404
Krista Marbury <laetaiustus@gmail.com>
2021-06-21 19:17:26 -0500

    added connection and index js files

--- sever.js
+++ sever.js
@@ -4,16 +4,80 @@
 
 const mysql = require("mysql");
 const inquirer = require("inquirer")
-const connection = mysql.createConnection({
-  host: "localhost",
-  // Your port; if not 3306
-  port: 3306,
-  // Your username
-  user: "root",
-  // Be sure to update with your own MySQL password!
-  password: "Typ3rG1rly",
-  database: "employees",
-});
+
+
+const viewAllEmployees = () => {
+  connection.query(" * from employee", (err, res) => {
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "rawlist",
+          choices() {
+            const choiceArray = [];
+            res.forEach(({ item_name }) => {
+              choiceArray.push(item_name);
+            });
+            return choiceArray;
+          },
+          message: "For whom do you look?",
+        },
+      ])
+      .then((answer) => {
+        connection.query(
+          "UPDATE auctions SET ? WHERE ?",
+          [
+            {
+              highest_bid: answer.bid,
+            },
+            {
+              item_name: answer.choice,
+            },
+          ],
+          (err, res) => {
+            if (err) throw err;
+            console.log("bid placed");
+          }
+        );
+      });
+  });
+};
+const createAuction = () => {
+  inquirer
+    .prompt([
+      {
+        name: "item",
+        type: "input",
+        message: "What is the item you would like to submit?",
+      },
+      {
+        name: "category",
+        type: "input",
+        message: "What category would you like to place your auction in?",
+      },
+      {
+        name: "startingBid",
+        type: "input",
+        message: "What would you like your starting bid to be?",
+      },
+    ])
+    .then((answer) => {
+      connection.query(
+        "INSERT INTO auctions SET ?",
+        {
+          item_name: answer.item,
+          category: answer.category,
+          starting_bid: answer.startingBid,
+          highest_bid: answer.startingBid,
+        },
+        (err, res) => {
+          if (err) throw err;
+          console.log(`${res.affectedRows} auction inserted!\n`);
+        }
+      );
+      bidAuction();
+    });
+};
 // loadprompts (inquirer choices)
 // selecting a choice will go to that function
 // view all employees
@@ -50,7 +114,7 @@ connection.connect((err) => {
   if (err) throw err;
   console.log(`connected as id ${connection.threadId}`);
   // loadprompts
-  // viewAllEmployees();
+  viewAllEmployees();
   // searchByDepartment();
   // searchByManager();
   readEmployees();
---
+++ sql_folder/connection.js
@@ -0,0 +1,10 @@
+const connection = mysql.createConnection({
+  host: "localhost",
+  // Your port; if not 3306
+  port: 3306,
+  // Your username
+  user: "root",
+  // Be sure to update with your own MySQL password!
+  password: "Typ3rG1rly",
+  database: "employees",
+});
